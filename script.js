a751c07a6af561b5d229d0d3388e1ba624ba695e
Simpleuser <124284281+simpleuserdontwatch@users.noreply.github.com>
2024-01-18 17:19:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//window.onbeforeunload = function (e) {
-//    e = e || window.event;
-//
-//    // For IE and Firefox prior to version 4
-//    if (e) {
-//        e.returnValue = 'Are you sure?';
-//    }
-//
-//    // For Safari
-//    return 'Are you sure?';
-//};
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Are you sure?';
+    }
+
+    // For Safari
+    return 'Are you sure?';
+};
 
 embed = document.getElementById("webview");
 code = document.getElementById("html");
