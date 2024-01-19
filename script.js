075e5a8f916756fd031ca62a3f7376ca796a8312
Simpleuser <kirillutusikov123@gmail.com>
2024-01-19 23:25:54 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,17 +12,45 @@ window.onbeforeunload = function (e) {
 
 embed = document.getElementById("webview");
 code = document.getElementById("html");
+code1 = document.getElementById("js");
+code2 = document.getElementById("css");
 rel = document.getElementById("reload");
+tabs = [
+  document.getElementById("tab1"),
+  document.getElementById("tab2"),
+  document.getElementById("tab3")
+]
 
 function writehtm() {
   embed.src = "about:blank";
   doc = embed.contentDocument || embed.contentWindow.document;
   doc.open();
   doc.write(code.value);
+  doc.write("<style>" + code2.value + "</style>");
+  doc.write("<script>" + code1.value + "</script>");
   doc.close();
 }
 
+function opentab(tab) {
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+  tabs[tab].style.display = "inline flex";
+}
+
 code.addEventListener("keyup", function(e) {
+  if (rel.checked) {
+    writehtm();
+  }
+});
+
+code1.addEventListener("keyup", function(e) {
+  if (rel.checked) {
+    writehtm();
+  }
+});
+
+code2.addEventListener("keyup", function(e) {
   if (rel.checked) {
     writehtm();
   }
@@ -43,7 +71,7 @@ function saveAs(blob, filename) {
 
 function view() {
   var win = window.open("about:blank#DONT-SHARE-THIS-LINK-IT-WONT-DISPLAY-FOR-OTHERS", "MyPage1");
-  win.document.body.innerHTML = code.value;
+  win.document.body.innerHTML = code.value+"<style>" + code2.value + "</style>"+"<script>" + code1.value + "</script>";
 }
 
 writehtm()
